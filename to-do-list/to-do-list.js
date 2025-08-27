658008d393e6db25dd24161309a7766bac9763f9
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-08-27 11:02:07 +0500

    Added a Delete Button to To Do list

--- to-do-list/to-do-list.js
+++ to-do-list/to-do-list.js
@@ -5,6 +5,20 @@ const list = document.getElementById('todo-list');
 addBtn.addEventListener('click', function() {
   const li = document.createElement('li');
   li.textContent = input.value;
+
   list.appendChild(li);
   input.value = '';
+
+  const deleteButton = () => {
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Delete';
+    list.appendChild(removeBtn);
+    
+    removeBtn.addEventListener('click', () => {
+      li.remove();
+      removeBtn.remove();
+    })
+  }
+
+  deleteButton();
 })
